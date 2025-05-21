7ba6dec66b7f6a708dabbab2da22ce7a99d97c32
zgdllt <limh24@mails.tsinghua.edu.cn>
2025-05-21 10:32:35 +0800

    nxt commit

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   // 选项...
-  publicPath: "./",
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/thu43-report/' // 替换为你的GitHub仓库名
+    : '/',
   outputDir: "docs",
 };
